e74d651ed13ccd23bfc8ba23fbcc2ab5ed5b7192
oniri1 <oniri1@naver.com>
2024-07-16 02:43:33 +0900

    axios prototype

--- navermap/src/Component/Map/NMap.tsx
+++ navermap/src/Component/Map/NMap.tsx
@@ -1,4 +1,7 @@
 import React, { useRef, useState, useEffect, useCallback } from "react";
+import axios, { AxiosResponse } from "axios";
+
+const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 const NMap = (): JSX.Element => {
   console.log("NMapFunc");
@@ -7,12 +10,13 @@ const NMap = (): JSX.Element => {
 
   const [map, setMap] = useState<naver.maps.Map>();
 
-  const [riderLng, setRiderLng] = useState<number>(127);
-  const [riderLat, setRiderLat] = useState<number>(35);
+  const [riderPosition, setRiderPosition] = useState<naver.maps.LatLng>();
+  const [, setRiderMarker] = useState<naver.maps.Marker>();
 
   const [userPosition, setUserPosition] = useState<naver.maps.LatLng>();
+  const [, setUserMarker] = useState<naver.maps.Marker>();
 
-  const [, setUserMarker] = useState<naver.maps.Marker>(); //유저 마커
+  const [boundPosition, setBoundPosition] = useState<naver.maps.LatLngBounds>();
 
   //func
   const userPositionSet = (address: string) => {
@@ -29,6 +33,7 @@ const NMap = (): JSX.Element => {
         if (response.v2.addresses.length > 0) {
           const result = response.v2.addresses[0];
 
+          console.log(result.y);
           setUserPosition(new naver.maps.LatLng(+result.y, +result.x));
         } else {
           console.error("유저 포지션 변환 오류");
@@ -36,6 +41,51 @@ const NMap = (): JSX.Element => {
       }
     );
   };
+
+  const getRiderLatLng = async () => {
+    await axios
+      .post(
+        `${serverUrl}/GpsRiderGet`,
+        {}, //아이디 추가해야 함
+        {}
+      )
+      .then((data: AxiosResponse<any, any>) => {
+        //성공시 콜백
+        //성공시 URL
+        console.log(data);
+      })
+      .catch(() => {
+        //실패시 URL
+        //실패시 URL
+        console.error("라이더 위치 실패");
+        //유저와 라이더의 위치
+        setRiderPosition(new naver.maps.LatLng(36.5, 127.5));
+      });
+  };
+
+  const getUserAddress = async () => {
+    await axios
+      .post(
+        `${serverUrl}/GpsUserGet`,
+        {}, //아이디 추가해야 함
+        {}
+      )
+      .then((data: AxiosResponse<any, any>) => {
+        //성공시 URL
+        console.log(data);
+      })
+      .catch(() => {
+        //실패시 URL
+        console.error("유저 어드레스 실패");
+        userPositionSet("서울특별시 강남구 테헤란로 152");
+      });
+  };
+  //
+
+  //riderGpsGet
+  useEffect(() => {
+    getRiderLatLng();
+  }, []);
 
   //userMaker
   useEffect(() => {
@@ -55,18 +105,45 @@ const NMap = (): JSX.Element => {
     }
   }, [userPosition]);
 
-  //main
+  //riderMaker
   useEffect(() => {
-    console.log("mount");
-
-    if (mapRef.current && naver) {
-      //네이버 맵에 사용할 Lat Lng를 설정해주는 코드
-      //유저와 라이더의 위치
-      const riderPosition: naver.maps.LatLng = new naver.maps.LatLng(
-        riderLat,
-        riderLng
+    if (riderPosition !== undefined) {
+      setRiderMarker(
+        new naver.maps.Marker({
+          position: riderPosition,
+          map: map,
+          icon: {
+            content:
+              '<img src="./imgs/HamsterRider.png" alt="" style="margin: 0px; padding: 0px; border: 2px solid blue; border-radius: 100%; display: block; max-width: none; max-height: none; -webkit-user-select: none; position: absolute; width: 35px; height: 35px; left: 0px; top: 0px; overflow: hidden;">',
+            size: new naver.maps.Size(22, 35),
+            anchor: new naver.maps.Point(11, 35),
+          },
+        })
       );
+    }
+  }, [riderPosition]);
 
+  //boundposition
+  useEffect(() => {
+    if (userPosition && riderPosition && map) {
+      console.log("bound posi reset");
+      setBoundPosition(
+        new naver.maps.LatLngBounds(riderPosition, userPosition)
+      );
+    }
+  }, [userPosition, riderPosition]);
+
+  //bunding
+  useEffect(() => {
+    if (map && boundPosition) {
+      console.log("bounding");
+      map.fitBounds(boundPosition);
+    }
+  }, [boundPosition]);
+
+  //mount
+  useEffect(() => {
+    if (mapRef.current && naver) {
       //new naver.maps.Map(타겟,옵션)을 넣는다. 타겟이 naverMap 옵션을 가지게 된다.
       setMap(
         new naver.maps.Map(mapRef.current, {
@@ -76,34 +153,9 @@ const NMap = (): JSX.Element => {
         })
       );
 
+      //유저 위치 얻기
+      getUserAddress();
       ///
-      const address = "서울특별시 강남구 테헤란로 152";
-      userPositionSet(address);
-      ///
-
-      // 두개를 보여줄 바운딩 포지션
-      if (
-        riderPosition !== undefined &&
-        userPosition !== undefined &&
-        map !== undefined
-      ) {
-        const boundPosition: naver.maps.LatLngBounds =
-          new naver.maps.LatLngBounds(riderPosition, userPosition);
-        map.fitBounds(boundPosition);
-      }
-
-      //마커를 생성하는 클래스 ({옵션들,좌표를 찍을 맵})
-      //라이더 마커
-      new naver.maps.Marker({
-        position: riderPosition,
-        map: map,
-        icon: {
-          content:
-            '<img src="./imgs/HamsterRider.png" alt="" style="margin: 0px; padding: 0px; border: 2px solid blue; border-radius: 100%; display: block; max-width: none; max-height: none; -webkit-user-select: none; position: absolute; width: 35px; height: 35px; left: 0px; top: 0px; overflow: hidden;">',
-          size: new naver.maps.Size(22, 35),
-          anchor: new naver.maps.Point(11, 35),
-        },
-      });
     }
   }, []);
 
